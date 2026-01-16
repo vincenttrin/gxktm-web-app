f5805cfc17ae2b5f26f57a2cf496223e9ba1a20a
vincenttrin <vince.trinh80@gmail.com>
2026-01-16 06:12:55 -0600

    started work on login page. Email signin is working

---
+++ frontend/src/app/login/page.tsx
@@ -0,0 +1,73 @@
+import { login, signup } from './actions'
+
+export default async function LoginPage(props: {
+  searchParams: Promise<{ message: string; error: string }>
+}) {
+  const searchParams = await props.searchParams
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="w-full max-w-md space-y-8">
+        <div>
+          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+          {searchParams?.message && (
+            <p className="mt-2 text-center text-sm text-green-600 font-medium">
+              {searchParams.message}
+            </p>
+          )}
+          {searchParams?.error && (
+            <p className="mt-2 text-center text-sm text-red-600 font-medium">
+              {searchParams.error}
+            </p>
+          )}
+        </div>
+        <form className="mt-8 space-y-6">
+          <div className="-space-y-px rounded-md shadow-sm">
+            <div>
+              <label htmlFor="email-address" className="sr-only">
+                Email address
+              </label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                required
+                className="relative block w-full rounded-t-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 px-3"
+                placeholder="Email address"
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">
+                Password
+              </label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 px-3"
+                placeholder="Password"
+              />
+            </div>
+          </div>
+
+          <div className="flex flex-col gap-4">
+            <button
+              formAction={login}
+              className="group relative flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm font-semibold text-white hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+            >
+              Sign in
+            </button>
+            <button
+              formAction={signup}
+              className="group relative flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 mb-2"
+            >
+              Sign up
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
